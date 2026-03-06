00f9290f99307874000538ea1e22fc0c7f946a59
N4nsh <nathanvez2@gmail.com>
2026-03-06 17:57:54 -0500

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // Set base to '/' for custom domain or '/repository-name/' for GitHub Pages
+  base: 'https://n4nsh.github.io/portfolio/',
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
